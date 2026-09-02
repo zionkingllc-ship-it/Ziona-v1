641d824ed56d0ed8cc41e81bd09f99d4d3f5d587
emmanuel-akinyemi-Dev <emizlife2014@gmail.com>
2026-09-03 00:26:08 +0100

    Fix: Reduce TwoButtonSwitch maxWidth from 200 to 230

--- components/ui/twoButtonSwitch.tsx
+++ components/ui/twoButtonSwitch.tsx
@@ -32,7 +32,7 @@ export default function TwoButtonSwitch({
       justifyContent="center"
 borderRadius={999}
     flex={1}
-    maxWidth={200}
+    maxWidth={230}
     >
       <Button
         flex={1}
